d1d0df1a1401b8ce82158ce22750cac359423256
HughParsonage <hugh.parsonage@gmail.com>
2016-01-07 21:19:36 +1100

    continue

--- dep_delay_analysis.R
+++ dep_delay_analysis.R
@@ -7,10 +7,16 @@ knitr::opts_chunk$set(fig.show = 'hide',
 
 ## ----loadPackages--------------------------------------------------------
 library(data.table)
+library(bit64)
 library(dplyr)
 library(magrittr)
 library(ggplot2)
+theme_update(text = element_text(family = "", 
+                                 face = "plain", colour = "black", size = 20, lineheight = 0.9, 
+                                 hjust = 0.5, vjust = 0.5, angle = 0, margin = margin(), 
+                                 debug = FALSE))
 library(nycflights13)  # for airports
+nycflights.airports <- airports
 library(fasttime)
 library(grattan)
 
@@ -109,9 +115,9 @@ Sys.time()
 
 ## ----Cleanse3-timezones--------------------------------------------------
 Sys.time()
-setkey(flights, DepDateTime)
-flights[,`:=`(DepDateTimeZulu = DepDateTime - lubridate::hours(tzOrigin),
-              ArrDateTimeZulu = ArrDateTime - lubridate::hours(tzDest) )]
+# setting keys doesn't improve timing
+flights[,`:=`(DepDateTimeZulu = DepDateTime - lubridate::hours(tzOrigin))]
+flights[,`:=`(ArrDateTimeZulu = ArrDateTime - lubridate::hours(tzDest))]
 Sys.time()
 
 ## ----Cleanse-weeks-------------------------------------------------------
@@ -138,6 +144,15 @@ flightsSanFran <- flightsSanFran[unique_dates]
 Sys.time()
 
 ## ----SanFran_Flights_by_date, results = 'hide'---------------------------
+maxN <- function(x, N=2){
+  len <- length(x)
+  if(N>len){
+    warning('N greater than length(x).  Setting N=length(x)')
+    N <- length(x)
+  }
+  sort(x,partial=len-N+1)[len-N+1]
+}
+
 setkey(unique_dates, Week)
 flightsSanFran %>%
   filter(!(Origin %in% c("SFO", "OAK") & Dest %in% c("SFO", "OAK"))) %>%
@@ -145,15 +160,27 @@ flightsSanFran %>%
                              Origin, 
                              Dest)) %>%
   count(Week, SF_airport) %>%
+  group_by(SF_airport) %>%
+  mutate(label.text = ifelse(n == maxN(n), paste(" ", SF_airport), NA_character_)) %>%
   setkey(Week) %>%
   data.table:::merge.data.table(unique(unique_dates)) %>%
   mutate(Date = fastPOSIXct(sprintf("%d-%02d-%02d", Year, Month, DayofMonth), tz = "GMT"),
          n = n) %>%  # not a sample
   ggplot(aes(x = Date, y = n, color = SF_airport, group = SF_airport)) + 
   geom_point() + 
+  geom_text(aes(label = label.text),
+            nudge_y = 0.5,
+            nudge_x = 1,
+            hjust = 0,
+            fontface = "bold",
+            size = 5) + 
+  theme(legend.position = "none") + 
   geom_line(size = 0.5) + 
   #
-  geom_vline(xintercept = as.numeric(as.POSIXct("2001-09-11")))
+  geom_vline(xintercept = as.numeric(as.POSIXct("2001-09-11"))) + 
+  scale_x_datetime(date_breaks = "5 years",
+                   date_labels = "%Y",
+                   minor_breaks = seq(as.POSIXct("1987-12-31"), as.POSIXct("2014-12-31"), by = "1 years"))
 
 ## ----SanFran_Flights_by_Carrier, fig.height = 9, outheight = "9in"-------
 carriers <- as.data.table(airlines)
@@ -184,7 +211,7 @@ flightsSanFran %>%
             size = 5) + theme(legend.position = "none") + 
   annotate("blank", x = 2019, y = 0) + 
   facet_grid(Origin ~ .) + 
-  theme(text = element_text(size = 16))
+  theme(text = element_text(size = 16)) 
 
 ## ----Volume-by-Carrier---------------------------------------------------
 top_5_carriers <- 
@@ -214,11 +241,122 @@ major_airports <-
   mutate(AirportOther = ifelse(TopN, Dest, "Other_airport")) %>%
   select(-n) %>%
   setkey(Dest) 
+
+airports_by_volume_by_year <- flights[major_airports][ ,.(n = .N * sample.weight.int), by = list(Year, AirportOther)] 
+
+airports_by_volume_by_2014 <- 
+  airports_by_volume_by_year %>%
+  filter(Year == 2014) %>%
+  filter(AirportOther != "AirportOther") %>%
+  merge(select(nycflights.airports, faa, name), by.x = "AirportOther", by.y = "faa") %>%
+  arrange(desc(n))
+gc(0,1)
+
 setkey(flights, Dest)
-flights[major_airports][ ,.(n = .N * sample.weight.int), by = list(Year, AirportOther)] %>%
-  filter(AirportOther != "Other_airport", Year > 1987L, Year < 2015L) %>%
-  ggplot(aes(x = Year, y = n, group = AirportOther, color = AirportOther)) + 
+gc(0,1)
+airports_by_volume_by_year %>%
+  filter(AirportOther != "Other_airport", Year > 1987L, Year < 2015L)  %>%
+  merge(select(nycflights.airports, faa, name), by.x = "AirportOther", by.y = "faa") %>%
+  mutate(name = factor(name, levels = airports_by_volume_by_2014$name)) %>%
+  ggplot(aes(x = Year, y = n, group = name, color = name)) + 
   geom_line() 
+gc(0,1)
+
+## ----Relative-volume-by-major-airports-----------------------------------
+rel_vol_major_airports <-
+  flights[major_airports][ ,.(n = .N * sample.weight.int), by = list(Year, AirportOther)] %>%
+  filter(AirportOther != "Other_airport", Year > 1987L, Year < 2015L) %>%
+  arrange(Year) %>%
+  group_by(AirportOther) %>%
+  mutate(rel = n/first(n)) %>%
+  merge(select(nycflights.airports, faa, name), by.x = "AirportOther", by.y = "faa")
+
+last_values <- 
+  rel_vol_major_airports %>%
+  filter(Year == max(Year)) %>%
+  arrange(rel) 
+
+rel_vol_major_airports  %>%
+  mutate(name = factor(name, levels = rev(last_values$name))) %>%
+  ggplot(aes(x = Year, y = rel, group = name, color = name)) + 
+  geom_line() 
+
+## ----airport-decoder-----------------------------------------------------
+otp201510 <- 
+  fread("../dep_delay/On_Time_On_Time_Performance_2015_10.csv")
+
+city_decoder <- 
+  otp201510 %>%
+  select(contains("Origin")) %>%
+  unique
+
+setkey(city_decoder, OriginCityMarketID)
+
+gc(T,T)
+city_market_decoder <- 
+  fread("../metadata//L_CITY_MARKET_ID.csv") %>%
+  setnames(old = c("Code", "Description"), 
+           new = c("OriginCityMarketID", "OriginCityMarketDescription")) %>%
+  setkey(OriginCityMarketID)
+city_market_decoder[,OriginCityMarketID := as.integer(OriginCityMarketID)]
+city_decoder <- merge(city_decoder, city_market_decoder, by = "OriginCityMarketID", all.x = TRUE, all.y = FALSE)
+gc(T,T)
+
+## ----Biggest-markets-out-of-SanFrancisco---------------------------------
+market_volume_by_year <-
+  flightsSanFran %>%
+  filter(Dest %in% c("SFO", "OAK")) %>%
+  merge(city_decoder, by = "Origin") %>%
+  count(Year, OriginCityMarketDescription) %>%
+  mutate(State = gsub("^.*([A-Z]{2}).*$", "\\1", OriginCityMarketDescription)) %>%
+  filter(n > 3650) %>%
+  mutate(Label = ifelse(Year == max(Year), OriginCityMarketDescription, NA_character_)) %>%
+  arrange(Year, desc(n)) 
+
+mkt.vol.by.yr <- function(year, colname){
+  magrittr::extract2(dplyr::filter(market_volume_by_year, Year == year), colname)
+}
+market_volume_by_year %>%
+  mutate(OriginCityMarketDescription = factor(OriginCityMarketDescription, levels = mkt.vol.by.yr(2015, "OriginCityMarketDescription"))) %>%
+  ggplot(aes(x = Year, y = n, color = OriginCityMarketDescription, group = OriginCityMarketDescription)) + 
+  #facet_grid(State ~ .) + 
+  geom_line() + 
+  #geom_text(aes(label = Label)) + 
+  #geom_dl(method = list("top.points", dl.trans(y = y+0.25), fontfamily = "bold"), aes(label = OriginCityMarketDescription)) + 
+  theme(legend.position = "none") -> p
+direct.label(p, list("top.points", dl.trans(y = y+0.25), fontface="bold"))
+  
+
+## ----aircraft------------------------------------------------------------
+FAA_aircraft <- 
+  fread("../met")
+
+## ----Cancellations-by-date-----------------------------------------------
+flights %>% 
+  group_by(Year, Month, DayofMonth) %>% 
+  summarise(prop_cancelled = mean(Cancelled)) %>% 
+  ggplot(aes(x = fasttime::fastPOSIXct(paste(Year, Month, DayofMonth, sep = "-")), y = prop_cancelled)) + 
+  geom_bar(stat = "identity", width=1)
+
+## ----Sept11-cumulative---------------------------------------------------
+ flights %>% 
+  group_by(Year, Month, DayofMonth) %>% 
+  summarise(prop_cancelled = mean(Cancelled)) %>% 
+  ungroup %>%
+  mutate(rank = dense_rank(prop_cancelled)) %>% 
+  ggplot(aes(x = jitter(rank, amount = 0.1), y = prop_cancelled)) + geom_bar(stat = "identity", width=1)
+
+## ----Sept11-airports-----------------------------------------------------
+flights %>%
+  filter(Year == 2001, Month == 9, DayofMonth == 11) %>%
+  group_by(Origin) %>%
+  summarise(latest_departure = max(DepDateTimeZulu)) %>%
+  ungroup %>%
+  arrange(latest_departure) %>% 
+  mutate(number_airports_closed = 1:n()) %>%
+  ggplot(aes(x = latest_departure, y = number_airports_closed)) + 
+  geom_line(group = 1) + 
+  geom_vline(xintercept = as.numeric(as.POSIXct("2001-09-11 09:17:00"))) 
 
 ## ----FINISHTIME----------------------------------------------------------
 FINISH.TIME <- Sys.time()
